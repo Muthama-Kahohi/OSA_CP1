51ee2d867fd145a43b19f6b0649d1c8c749a0ba8
Muthama-Kahohi <paul.kahohi@andela.com>
2016-12-08 11:21:04 +0300

    Adds persons tests

--- project/app/amity.py
+++ project/app/amity.py
@@ -1,10 +1,17 @@
 class Amity(object):
 
     def __init__(self):
+        self.rooms = {}
         self.rooms_list = []
+        self.office_list = []
+        self.living_list = []
+        self.persons_list = []
         pass
 
     def create_room(self, *args):
+        pass
+
+    def create_person(self, *args):
         pass
 
 
--- project/tests/osatests.py
+++ project/tests/osatests.py
@@ -1,80 +1,84 @@
 import unittest
-from app.amity import Amity, Rooms, LivingSpace, Office
-from app.people import Person, Fellow, Staff
+from app.Amity import Amity
 
 
 class OfficeSpaceAllocationTests(unittest.TestCase):
 
     def setUp(self):
         self.facility = Amity()
-        self.room = Rooms()
-        self.space = LivingSpace()
-        self.office = Office()
-        self.Person = Person()
-        self.fellow = Fellow()
-        self.staff = Staff()
+        self.room = Room()
 
-    # First four tests test for inheritance
-    def test_create_single_office_room(self):
+    def test_create_single_office(self):
         '''Tests creating a single office room'''
+        self.assertEqual(0, self.facility.rooms_list)
         self.facility.create_room(
             {"room_type": "office", "room_name": "Krypton"})
         self.assertEqual(1, len(self.facility.rooms_list))
 
     def test_create_single_living_room(self):
         ''''Tests creating a single living space'''
+        self.assertEqual(0, self.facility.rooms_list)
         self.facility.create_room(
             {"room_type": "living space", "room_name": "PHP"})
         self.assertEqual(1, len(self.facility.rooms_list))
 
     def test_create_multiple_offices(self):
+        self.assertEqual(0, self.facility.rooms_list)
         self.facility.create_room(
             {"room_type": "offices",
                 "room_name": ["Hogwarts", "Bumblebee", "Cyan"]})
         self.assertEqual(3, len(self.facility.rooms_list))
 
     def test_create_multiple_living_spaces(self):
+        self.assertEqual(0, self.facility.rooms_list)
         self.facility.create_room(
             {"room_type": "living space",
                 "room_name": ["Java", "Python", "Android"]})
         self.assertEqual(3, len(self.facility.rooms_list))
 
-    def test_parameters_passed_are_strings(self):
-        """Ensures all the parameterts passeds are strings """
-        self.assertRaises(
-            ValueError, self.facility.create_room, 111, 444, 555)
+    def test_create_person(self):
+        '''Asserts that person is added to a list and they are in the list'''
 
-        self.assertRaises(
-            ValueError, self.facility.create_room, "Offices", 444, 555)
+        s_fname = "Percila"
+        f_fname = "Paul"
+        self.assertEqual(0,
+                         len(self.facility.persons_list))
+        self.facility.create_person(f_fname, "Kahohi", "Fellow", "Y")
+        self.facility.create_person(s_fname, "Njira", "Staff", "N")
+        self.assertEqual(2,
+                         len(self.facility.persons_list))
+        self.assertIn(s_fname, self.facility.persons_list)
+        self.assertIn(f_fname, self.facility.persons_list)        
 
-    def test_test_room_type_is_livingspace_or_room(self):
 
-        self.facility.create_room(
-            {"room_type": "living space",
-                "room_name": ["Java", "Python", "Android"]})
+        
 
-        self.assertIn(
-            self.facility.rooms_list[0]["room_name"], self.facility.room_type)
+    def test_person_values_added_are_of_correct_types(self):
 
-    def test_add_person(self):
-        '''Test that a person is added'''
-        self.facility.add_person(
-            {"first_name": "Paul", "second_name": "Kahohi",
-             "Title": "Fellow", "wants_accomodation": "Y"})
-        self.assertEqual(1, len(self.facility.persons_list))
+        self.assertEqual(0,
+                        len(self.facility.persons_list))
+        self.assertRaises(ValueError,
+            self.facility.create_person,456, "Muthama", "Fellow","Y" )
+        self.assertRaises(ValueError,
+            self.facility.create_person,"Paul", 89, "Fellow","Y" )
+        self.assertRaises(ValueError,
+            self.facility.create_person,"percila", "Njira", 768,"Y" )
+        self.assertRaises(ValueError,
+            self.facility.create_person,"Percila", "Njira", "Fellow", 0 )     
 
-    def test_person_details_are_strings(self):
-        """Ensures the details added are strings"""
-        self.assertRaises(ValueError, self.facility.add_person,
-                          "paul", 763, "fellow", 1)
-
-        self.assertRaises(ValueError, self.facility.add_person,
-                          453, 763, 34, 7)
-
-    def test_person_allocation(self):
+    def test_add_person_to_room(self):
+        
             
 
-
+    def test_rellocate(self):
+        '''
+        1. create person
+        2. create room
+        3. remove person from room
+        4. confirm person removed from room
+        5. Add person to another room
+        6. confirm person is added to that particular room
+        '''
 
 
 if __name__ == '__main__':
