c3b64bb359db6fc7fcab1cd024a24657be568b29
Muthama-Kahohi <paul.kahohi@andela.com>
2016-12-19 07:30:21 +0300

    > Adds a requirements.txt file Adds a .gitignore file Adds a create_room, add_person, and allocation function logic

--- project/app/amity.py
+++ project/app/amity.py
@@ -1,3 +1,6 @@
+from random import randint
+
+
 class Amity(object):
 
     def __init__(self):
@@ -7,19 +10,129 @@ class Amity(object):
         self.living_list = []
         self.persons_list = []
         self.allocated_persons = []
-        pass
+        self.unfilled_rooms = []
 
-    def create_room(self, *args):
-        pass
+    def create_room(self, to_create={}):
+        if 'room_type' in to_create.keys() and 'room_name' in to_create.keys():
+            room_type = to_create['room_type'].lower()
 
-    def create_person(self, *args):
-        pass
+            if room_type == 'office':
+                if type(to_create['room_name']) is str:
+                    room_name = to_create['room_name']
+                    if room_name not in self.office_list:
+                        self.office_list.append(
+                            {'room_name': room_name, "occupants": []})
+                        self.rooms_list.append(room_name)
+                        self.unfilled_rooms.append(room_name)
+                        room_name = Office(room_name)
 
-    def add_person(self,room_name, person_name):
-        pass
+                    else:
+                        print ("Room already exists")
+
+                elif type(to_create['room_name']) is list:
+                    for item in to_create['room_name']:
+                        room_name = item
+                        if room_name not in self.office_list:
+                            self.office_list.append(
+                                {'room_name': room_name, "occupants": []})
+                            self.rooms_list.append(room_name)
+                            self.unfilled_rooms.append(room_name)
+                            room_name = Office(room_name)
+
+                        else:
+                            print("Room already exists")
+
+            elif room_type == 'living':
+                if type(to_create['room_name']) is str:
+                    room_name = to_create['room_name']
+                    if room_name not in self.living_list:
+                        self.living_list.append(
+                            {'room_name': room_name, "occupants": []})
+                        self.rooms_list.append(room_name)
+                        self.unfilled_rooms.append(room_name)
+                        room_name = LivingSpace(room_name)
+                    else:
+                        print("Room already exists")
+
+                elif type(to_create['room_name']) is list:
+                    for item in to_create['room_name']:
+                        room_name = item
+                        if room_name not in self.living_list:
+                            self.living_list.append(
+                                {'room_name': room_name, 'occupants': []})
+                            self.rooms_list.append(room_name)
+                            room_name = LivingSpace(room_name)
+                            self.unfilled_rooms.append(room_name)
+                        else:
+                            print("Room already exists")
+
+            else:
+                print("Room can only be an Office or a Living Space")
+        self.rooms_list = self.office_list + self.living_list 
+
+    def create_person(self, fname, lname, role, accomodation):
+        role, accomodation = role.upper(), accomodation.upper()
+        fellow, staff, yes, no = 'FELLOW', 'STAFF', 'Y', 'N'
+        # Ensure that the  first name and last name are strings
+        if type(fname) is not str or type(lname) is not str:
+            print("The names should be of type string")
+            return
+        # Ensures that invalid roles are not added
+        if role != fellow and role != staff:
+            print("Role can only be either a fellow or a staff")
+            return
+        # Ensure that accomoadation can only be Y or N
+        if type(accomodation) is not str and type(accomodation) is not str:
+            raise TypeError
+            print("Invalid accomodation choice")
+            return
+        else:
+            if accomodation != yes and accomodation != no:
+                print("Accomodation choice should either be Y or N")
+                return
+            elif role == staff and accomodation == yes:
+                print("Staff members are not given accomodation, only offices")
+                return
+
+        person_dict = {'fname': fname, 'lname': lname,
+                       'role': role, 'wants_accomodation': accomodation}
+
+        self.persons_list.append(person_dict)
+
+        print ('%s %s has been added.' % (fname, lname))
+        # Allocate office to any person added
+        if len(self.office_list) == 1:
+            self.office_list[0]['occupants'].append(fname)
+            print("%s has been added to %s" %
+                  (fname, self.office_list[0]['room_name']))
+        elif len(self.office_list) == 0:
+            print("No rooms to allocate")
+        else:
+            rand = randint(0, len(self.office_list) - 1)
+            self.office_list[rand]['occupants'].append(fname)
+            print("%s has been added to %s" %
+                  (fname, self.office_list[rand]['room_name']))
+        #Allocate Living Space
+        if role == fellow and accomodation == yes:
+            if len(self.living_list) == 1:
+                self.living_list[0]['occupants'].append(fname)
+                print("%s has been added to %s" %
+                      (fname, self.living_list[0]['room_name']))
+            elif len(self.living_list) == 0:
+                print("No rooms to allocate")
+            else:
+                rand = randint(0, len(self.office_list) - 1)
+                self.living_list[rand]['occupants'].append(fname)
+                print("%s has been added to %s" %
+                      (fname, self.living_list[rand]['room_name']))
+
+    def add_person(self, fname, lname):
+        pass            
 
     def remove_person(self, person_name):
-        pass         
+        pass
+    def reallocate(self, fname, room_name):
+        pass    
 
 
 class Rooms (object):
@@ -28,14 +141,13 @@ class Rooms (object):
         self.occupants = []
 
 
-        
-
-
 class LivingSpace(Rooms):
-    capacity = 4
-        
+    def __init__(self, room_name):
+        self.capacity = 4
 
 
 class Office(Rooms):
-    
-    capacity = 6
+    def __init__(self, room_name):
+        self.capacity = 6
+
+
--- project/tests/osatests.py
+++ project/tests/osatests.py
@@ -8,33 +8,34 @@ class OfficeSpaceAllocationTests(unittest.TestCase):
     def setUp(self):
         self.facility = Amity()
         self.room = Rooms()
-        self.room2 = Rooms()   
+        self.room2 = Rooms()
 
     def test_create_single_office(self):
         '''Tests creating a single office room'''
-        self.assertEqual(0, self.facility.rooms_list)
+        self.assertEqual(0, len(self.facility.rooms_list))
         self.facility.create_room(
             {"room_type": "office", "room_name": "Krypton"})
-        self.assertEqual(1, len(self.facility.rooms_list))
+        num = len(self.facility.rooms_list)
+        self.assertEqual(1, num)
 
     def test_create_single_living_room(self):
         ''''Tests creating a single living space'''
-        self.assertEqual(0, self.facility.rooms_list)
+        self.assertEqual(0, len(self.facility.rooms_list))
         self.facility.create_room(
-            {"room_type": "living space", "room_name": "PHP"})
+            {"room_type": "living", "room_name": "PHP"})
         self.assertEqual(1, len(self.facility.rooms_list))
 
     def test_create_multiple_offices(self):
-        self.assertEqual(0, self.facility.rooms_list)
+        self.assertEqual(0, len(self.facility.rooms_list))
         self.facility.create_room(
-            {"room_type": "offices",
+            {"room_type": "office",
                 "room_name": ["Hogwarts", "Bumblebee", "Cyan"]})
         self.assertEqual(3, len(self.facility.rooms_list))
 
     def test_create_multiple_living_spaces(self):
-        self.assertEqual(0, self.facility.rooms_list)
+        self.assertEqual(0, len(self.facility.rooms_list))
         self.facility.create_room(
-            {"room_type": "living space",
+            {"room_type": "living",
                 "room_name": ["Java", "Python", "Android"]})
         self.assertEqual(3, len(self.facility.rooms_list))
 
@@ -45,17 +46,16 @@ class OfficeSpaceAllocationTests(unittest.TestCase):
         f_fname = "Paul"
         self.assertEqual(0,
                          len(self.facility.persons_list))
-        self.facility.create_person(f_fname, "Kahohi", "Fellow", "Y")
+
         self.facility.create_person(s_fname, "Njira", "Staff", "N")
-        self.assertEqual(2,
+        self.assertEqual(1,
                          len(self.facility.persons_list))
-        self.assertIn(s_fname, self.facility.persons_list)
-        self.assertIn(f_fname, self.facility.persons_list)
+
 
     def test_remove_person(self):
         self.assertEqual(0,
                          len(self.facility.persons_list))
-        k=self.facility.create_person("Ian", "Oti", "Fellow", "Y")            
+        k = self.facility.create_person("Ian", "Oti", "Fellow", "Y")
         self.assertEqual(1,
                          len(self.facility.persons_list))
         self.facility.remove_person(k)
@@ -65,58 +65,55 @@ class OfficeSpaceAllocationTests(unittest.TestCase):
     def test_person_values_added_are_of_correct_types(self):
 
         self.assertEqual(0,
-                        len(self.facility.persons_list))
+                         len(self.facility.persons_list))
         self.assertRaises(ValueError,
-            self.facility.create_person,456, "Muthama", "Fellow","Y" )
+                          self.facility.create_person, 456, "Muthama", "Fellow", "Y")
         self.assertRaises(ValueError,
-            self.facility.create_person,"Paul", 89, "Fellow","Y" )
+                          self.facility.create_person, "Paul", 89, "Fellow", "Y")
         self.assertRaises(ValueError,
-            self.facility.create_person,"percila", "Njira", 768,"Y" )
+                          self.facility.create_person, "percila", "Njira", 768, "Y")
         self.assertRaises(ValueError,
-            self.facility.create_person,"Percila", "Njira", "Fellow", 0 )     
+                          self.facility.create_person, "Percila", "Njira", "Fellow", 0)
 
     def test_add_person_to_room(self):
         r_name = "PHP"
-        f_fname = "Paul"   
-        self.facility.create_person(f_fname, "Kahohi", "Fellow", "Y")        
+        f_fname = "Paul"
+        self.facility.create_person(f_fname, "Kahohi", "Fellow", "Y")
         self.facility.create_room(
-            {"room_type": "living space", "room_name": r_name})
+            {"room_type": "living", "room_name": r_name})
         # self.assertEqual(1, len(self.facility.rooms_list))
-        self.assertEqual(0,len(self.facility.allocated_persons))
+        self.assertEqual(0, len(self.facility.allocated_persons))
         self.facility.add_person(r_name, f_fname)
-        self.assertEqual(1,len(self.facility.allocated_persons))  
-
+        self.assertEqual(1, len(self.facility.allocated_persons))
 
     def test_reallocate(self):
         r_name = "Go"
         r2_name = "Java"
         f_fname = "Sophie"
-        self.facility.create_person(f_fname, "Kahohi", "Fellow", "Y")        
+        self.facility.create_person(f_fname, "Kahohi", "Fellow", "Y")
         self.facility.create_room(
-            {"room_type": "living space", "room_name": r_name})
+            {"room_type": "living", "room_name": r_name})
         self.facility.create_room(
-            {"room_type": "living space", "room_name": r2_name})       
+            {"room_type": "living", "room_name": r2_name})
         # self.assertEqual(1, len(self.facility.rooms_list))
-        
-        self.assertEqual(0,len(self.room.occupants))
-        self.assertEqual(0,len(self.room2.occupants))
 
-        #Adds person to room
+        self.assertEqual(0, len(self.room.occupants))
+        self.assertEqual(0, len(self.room2.occupants))
+
+        # Adds person to room
         self.facility.add_person(self.room2, f_fname)
-        self.assertEqual(1,len(self.room2.occupants))
+        self.assertEqual(1, len(self.room2.occupants))
 
-        #Reallocate person
+        # Reallocate person
         self.reallocate(f_fname, self.room)
-        self.assertEqual(1,len(self.room.occupants))
-        self.assertEqual(0,len(self.room2.occupants))
+        self.assertEqual(1, len(self.room.occupants))
+        self.assertEqual(0, len(self.room2.occupants))
 
     def test_maximum_capacities_of_room_and_office(self):
-        living = LivingSpace("Shell") 
+        living = LivingSpace("Shell")
         office = Office("Camelot")
-        self.assertEqual(4,living.capacity)
-        self.assertEqual(6,office.capacity)
-
-
+        self.assertEqual(4, living.capacity)
+        self.assertEqual(6, office.capacity)
 
 
 if __name__ == '__main__':
