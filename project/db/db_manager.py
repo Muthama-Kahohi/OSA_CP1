87592e73d4aa88cee1dd505d4013bcc3baf6c718
Muthama-Kahohi <paul.kahohi@andela.com>
2016-12-22 21:57:52 +0300

    Puts brackets on the sqlalchemy imports

--- project/db/db_manager.py
+++ project/db/db_manager.py
@@ -1,5 +1,5 @@
 import sys
-from sqlalchemy import create_engine, Column, Integer, ForeignKey, String, TEXT, BLOB
+from sqlalchemy import (create_engine, Column, Integer, ForeignKey, String)
 from sqlalchemy.orm import sessionmaker, relationship
 
 from sqlalchemy.ext.declarative import declarative_base
